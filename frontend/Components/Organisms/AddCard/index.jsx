89e43df54e6354d5e278ed91fa708cb124db7b64
Tomasz Lisowski <t.lisowski22@gmail.com>
2019-08-16 08:00:26 +0300

    Create basic user card horizontal scroll view

---
+++ frontend/Components/Organisms/AddCard/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Text from '../../Atoms/Text/index.jsx'
+import './index.css'
+
+export default class AddCard extends React.Component {
+  render () {
+    return (
+      <div className={'AddCard'}><button>Add</button></div>
+    )
+  }
+}
--- frontend/Index.jsx
+++ frontend/Index.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Axios from 'axios'
+import './Index.css'
 
-class Index extends React.Components {
+import UserCard from './Components/Organisms/UserCard/index.jsx'
+import AddCard from './Components/Organisms/AddCard/index.jsx'
+
+class Index extends React.Component {
+  constructor() {
+    super()
+    this.getUsers = this.getUsers.bind(this)
+    this.state = {
+      users: [],
+      userElements: []
+    }
+    this.getUsers()
+  }
+  getUsers() {
+    axios.get('/users')
+    .then(function (response) {
+      // handle success
+      this.state.users = response
+      console.log(response)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error)
+    })
+    .finally(function () {
+      // always executed
+    })
+    this.state.userElements = this.state.users.map((element) => {
+      return(<UserCard
+      name={element.name}
+      accessToken={element.accessToken}
+      key={`Usercard-${name}`}
+      />)
+    })
+  }
   render() {
-    return(<h1>Test</h1>)
+    return(<div className={'Index'}>{this.state.userElements}<AddCard/></div>)
   }
 }
 
