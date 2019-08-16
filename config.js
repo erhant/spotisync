af78b0df8c1b9f271273602d8c756f07c491b5b0
Erhan Tezcan <erhant96@hotmail.com>
2019-08-16 12:28:22 +0300

    Goodbye frontend

--- config.js
+++ config.js
@@ -4,10 +4,11 @@ const globals = {
   expressPort: 8080,
   clientId: '4c26f8ba0e80403b8565a8f78dc2230c',
   clientSecret: '6a216c412b6747648a6bc1a5fb5ca83a',
-  redirectURI: 'http://localhost/callback',
+  redirectURI: 'http://localhost:8080/callback',
   scopes: 'user-read-email user-read-private user-modify-playback-state',
   tokenURI: 'https://accounts.spotify.com/api/token',
-  stateKey: 'spotify_auth_state'
+  stateKey: 'spotify_auth_state',
+  expressURL: 'http://localhost:8080'
 }
 
 export { globals }
