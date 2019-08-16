2f115b054d9f0c4e5a1342450b757064ed6a2112
Tomasz Lisowski <t.lisowski22@gmail.com>
2019-08-16 04:51:44 +0300

    Initialize frontend

---
+++ frontend/Index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Index extends React.Components {
+  render() {
+    return(<h1>Test</h1>)
+  }
+}
+
+ReactDOM.render(<Index/>, document.getElementById('root'))
