c74142ea3144913d1d46201d246506d4577c369e
Tomasz Lisowski <t.lisowski22@gmail.com>
2019-08-16 03:51:28 +0300

    Add refresh token function

--- functions/utils.js
+++ functions/utils.js
@@ -9,11 +9,29 @@ export function checkToken (token) {
     headers: { 'Authorization': 'Bearer ' + token},
     json: true
   }
-  request.get(options, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
+  request.get(options, (error, res, body) => {
+    if (!error && res.statusCode === 200) {
       return true
     } else {
       return false
+    }
+  })
+}
+
+export function refreshToken (refreshToken, client_id, client_secret) {
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  }
+
+  request.post(authOptions, (error, res, body) => {
+    if (!error && response.statusCode === 200) {
+      return body.access_token
     }
   })
 }
