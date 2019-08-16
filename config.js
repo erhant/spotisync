231eec7bc853614a971c0746e674d6f0d49299ee
Erhan Tezcan <erhant96@hotmail.com>
2019-08-16 23:41:59 +0300

    Isolated config file

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
 "use strict"
 
+import { secrets } from './secretConfig.js'
+
 const globals = {
-  expressPort: 8080,
-  clientId: '4c26f8ba0e80403b8565a8f78dc2230c',
-  clientSecret: '6a216c412b6747648a6bc1a5fb5ca83a',
-  redirectURI: 'http://localhost:8080/callback',
+  expressPort: 80,
+  clientId: secrets.clientId,
+  clientSecret: secrets.clientSecret,
+  redirectURI: 'http://localhost/callback',
   scopes: 'user-read-email user-read-private user-modify-playback-state',
   tokenURI: 'https://accounts.spotify.com/api/token',
   stateKey: 'spotify_auth_state',
-  expressURL: 'http://localhost:8080'
+  expressURL: 'http://localhost'
 }
 
 export { globals }
