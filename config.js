e28870f6e4bfc099bb828f2f42e9ca8f72b4ec4d
Erhan Tezcan <erhant96@hotmail.com>
2019-08-16 04:37:46 +0300

    Authenticator (use localhost/login)

--- config.js
+++ config.js
@@ -1,7 +1,13 @@
-"use strict"
+'use strict'
 
 const globals = {
-  expressPort: 80
+  expressPort: 80,
+  clientId: '4c26f8ba0e80403b8565a8f78dc2230c',
+  clientSecret: '6a216c412b6747648a6bc1a5fb5ca83a',
+  redirectURI: 'http://localhost/callback',
+  scopes: 'user-read-email user-read-private ',
+  tokenURI: 'https://accounts.spotify.com/api/token',
+  stateKey: 'spotify_auth_state'
 }
 
 export { globals }
--- functions/authenticator.js
+++ functions/authenticator.js
@@ -1,5 +1,81 @@
-"use strict"
+'use strict'
 
-export function authenticator () {
+import { globals } from './../config.js'
+import querystring from 'querystring'
+import request from 'request'
+
+const clientId = globals.clientId // Your client id
+const clientSecret = globals.clientSecret // Your secret
+const redirectURI = globals.redirectURI // Your redirect uri
+const scope = globals.scope // Your scope
+const stateKey = globals.stateKey // TODO: transfer this to global?
+
+// TODO: move this to utils
+function generateRandomString (length) {
+  let text = ''
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  for (let i = 0; i < length; i++) { text += possible.charAt(Math.floor(Math.random() * possible.length)) }
+  return text
+}
+
+export function authenticator (req, res) {
   console.log('Authenticator function was called')
+
+  const state = generateRandomString(16)
+  res.cookie(stateKey, state)
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: clientId,
+      scope: scope,
+      redirect_uri: redirectURI,
+      state: state
+    }))
+}
+
+export function tokenRetriever (req, res) {
+  console.log('i start token retrival')
+  const code = req.query.code || null
+  const state = req.query.state || null
+  const storedState = req.cookies ? req.cookies[stateKey] : null
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+        querystring.stringify({
+          error: 'state_mismatch'
+        }))
+  } else {
+    res.clearCookie(stateKey)
+    const authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirectURI,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        Authorization: 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
+      },
+      json: true
+    }
+
+    request.post(authOptions, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        const accessToken = body.access_token
+        const refreshToken = body.refresh_token
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { Authorization: 'Bearer ' + accessToken },
+          json: true
+        }
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function (error, response, body) {
+          console.log(body)
+        })
+      }
+    })
+  }
 }
