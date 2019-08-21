dd24b630a8af74d7930266870c23f60130f03fdb
Aerophare263 <t.lisowski22@gmail.com>
2019-08-21 15:34:06 +0200

    Add empty track queue component

---
+++ src/Components/TrackQueue.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class TrackQueue extends React.Component {
+  render () {
+    return (
+      <div>
+        <p>Track Queue</p>
+      </div>
+    )
+  }
+}
