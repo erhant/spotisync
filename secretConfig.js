aa869b8c3e1c805863e238d4dbc1a5b6ec895bfe
erhantezcan <erhant96@hotmail.com>
2020-06-27 16:53:45 +0300

    backend is dead, long live frontend

--- secretConfig.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-const secrets = {
-  clientId: '4c26f8ba0e80403b8565a8f78dc2230c',
-  clientSecret: '6a216c412b6747648a6bc1a5fb5ca83a',
-  redirectURI: 'http://localhost/callback'
-}
-
-export { secrets }
